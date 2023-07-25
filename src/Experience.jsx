2f299514eea427520d01e3564cdab5f995a022b9
srikar-pasumarthy <srikarp@email.unc.edu>
2023-07-25 17:22:51 -0400

    Added laptop model and shadow.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,24 @@
-import { OrbitControls } from '@react-three/drei'
+import { useGLTF, OrbitControls, Environment, Float, ContactShadows, Center } from '@react-three/drei'
 
 export default function Experience()
 {
+
+    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+
     return <>
+        {/** Set up the controls and background */}
+        <color args={['#241a1a']} attach="background" />
+        <OrbitControls maxAzimuthAngle={Math.PI/2} minAzimuthAngle={-Math.PI/2}/>
+        <Environment preset="city" />
 
-        <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        {/** Add laptop to the scene and related elements */}
 
+            <Float rotationIntensity={0.4} >
+                <primitive
+                    object={computer.scene}
+                    position-y={-1.2}
+                />
+            </Float>
+        <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4}/>
     </>
 }
