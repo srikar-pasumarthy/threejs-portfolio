f118ec7c7c9a82f6fd60738b07f5c79f3e61f25e
srikar-pasumarthy <srikarp@email.unc.edu>
2023-07-31 10:16:25 -0400

    Fixed bug on mobile.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,18 +1,29 @@
-import { useGLTF, OrbitControls, Environment, Float, ContactShadows, Html, Text } from '@react-three/drei'
+import { useGLTF, OrbitControls, Environment, Float, ContactShadows, Html, Text} from '@react-three/drei'
+import { useThree } from '@react-three/fiber';
 
 export default function Experience()
 {
 
     const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+    // <OrbitControls maxAzimuthAngle={Math.PI/2} minAzimuthAngle={-Math.PI/2} enablePan={false} maxPolarAngle={Math.PI/2}/>
+
+    const {viewport} = useThree()
+    const isMobile = viewport.width < 5;
+    console.log(viewport.width)
+
+    // Adjust positions based on the screen size
+    const laptopPositionY = isMobile ? -1.2 : -1.4;
+    const htmlScreenPositionY = isMobile ? 1.4 : 1.6;
+    const htmlScreenPositionZ = isMobile ? -1.5 : -1.4;
 
     return <>
         {/** Set up the controls and background */}
-        <color args={['#241a1a']} attach="background" />
-        <OrbitControls maxAzimuthAngle={Math.PI/2} minAzimuthAngle={-Math.PI/2} enablePan={false} maxPolarAngle={Math.PI/2}/>
+        <color args={['#FFFFFF']} attach="background" />
+        <OrbitControls />
         
 
         {/** Add laptop to the scene and related elements */}
-        <Float rotationIntensity={0.4} >
+        <Float rotationIntensity={0} >
             <rectAreaLight
                     width={ 2.5 }
                     height={ 1.65 }
@@ -21,15 +32,15 @@ export default function Experience()
                     rotation={ [ - 0.1, Math.PI, 0 ] }
                     position={ [ 0, 0.55, - 1.15 ] }
             />
-            <primitive object={computer.scene} position-y={ - 1.2 }>
+            <primitive object={computer.scene} position-y={laptopPositionY}>
                 <Html
                         transform
                         wrapperClass="htmlScreen"
                         distanceFactor={ 1.17 }
-                        position={ [ 0, 1.56, - 1.4 ] }
+                        position={ [ 0, htmlScreenPositionY, htmlScreenPositionZ ] }
                         rotation-x={ - 0.256 }
                     >
-                        <iframe src="https://srikar-pasumarthy.github.io/water-reflection/dist/index.html" />
+                    <iframe src="https://srikar-pasumarthy.github.io/water-reflection/dist/index.html" />
                 </Html>
             </primitive>
         </Float>
